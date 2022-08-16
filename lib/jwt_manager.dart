7393104466fd9edb25a3e7605156cef15cfdd797
cyrax <bskaxe@gmail.com>
2022-08-16 17:01:52 +0700

    export

--- lib/jwt_manager.dart
+++ lib/jwt_manager.dart
@@ -4,5 +4,10 @@
 library jwt_manager;
 
 export 'src/signature_verifier.dart';
+export 'src/jwt_builder.dart';
+export 'src/key_parser.dart';
+export 'src/signifier.dart';
+export 'src/token.dart';
+
 
 // TODO: Export any libraries intended for clients of this package.
--- test/jwt_manager_test.dart
+++ test/jwt_manager_test.dart
@@ -1,4 +1,1 @@
-import 'package:jwt_manager/jwt_manager.dart';
-import 'package:test/test.dart';
-
 void main() {}
