8a14c393eca7c60cce0b75a88e745472365afafa
cyrax <bskaxe@gmail.com>
2022-08-16 17:28:07 +0700

    feat: add reissue token

--- lib/src/token.dart
+++ lib/src/token.dart
@@ -1,3 +1,4 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
 abstract class TokenDto {
   Map<String, Object> buildHeader();
   Map<String, Object> buildClaims();
@@ -40,5 +41,30 @@ class FcmTokenDto implements TokenDto {
       "exp": exp.millisecondsSinceEpoch ~/ 1000,
       "iat": iat.millisecondsSinceEpoch ~/ 1000,
     };
+  }
+
+  FcmTokenDto reissue() {
+    final now = DateTime.now();
+    return copyWith(iat: now, exp: now.add(const Duration(hours: 1)));
+  }
+
+  FcmTokenDto copyWith({
+    String? alg,
+    String? typ,
+    String? iss,
+    String? scope,
+    String? aud,
+    DateTime? exp,
+    DateTime? iat,
+  }) {
+    return FcmTokenDto(
+      alg: alg ?? this.alg,
+      typ: typ ?? this.typ,
+      iss: iss ?? this.iss,
+      scope: scope ?? this.scope,
+      aud: aud ?? this.aud,
+      exp: exp ?? this.exp,
+      iat: iat ?? this.iat,
+    );
   }
 }
